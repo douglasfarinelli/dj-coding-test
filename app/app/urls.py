e21f1a3758dc72fd329ed175d71311bfa542b458
Douglas Farinelli <douglas.farinelli@dressandgo.com.br>
2017-07-01 03:39:46 -0300

    Setting urls.py and urls_api.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from . import urls_api
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', admin.site.urls),
+    url(r'^api/', include([
+        url(r'^v1/', include(urls_api.V1_ROUTES.get_urls(), namespace='v1')),
+    ])),
 ]
--- app/app/urls_api.py
+++ app/app/urls_api.py
@@ -1,2 +1,5 @@
 # ! coding: utf-8
 
+from rest_framework import routers
+
+V1_ROUTES = routers.DefaultRouter()
