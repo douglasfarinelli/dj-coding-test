501abb88f7ee740331ae42a86a416722ed53196d
Douglas Farinelli <douglas.farinelli@dressandgo.com.br>
2017-07-01 03:42:23 -0300

    Alternative to auto-load DRF routes

--- app/core/apps.py
+++ app/core/apps.py
@@ -1,2 +1,15 @@
 # ! coding: utf-8
 
+import contextlib
+from django.utils import module_loading
+
+
+class ApiAppConfigMixin:
+    """Mixin to load DRF routes defined in app_name.<API_ROUTES_PATH>"""
+
+    API_ROUTES_PATH = 'api.routes'
+
+    def ready(self):
+        super(ApiAppConfigMixin, self).ready()
+        with contextlib.suppress(ImportError):
+            module_loading.import_module('%s.%s' % (self.name, self.API_ROUTES_PATH))
