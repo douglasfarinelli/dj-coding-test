40ca7978ce9198391c5769ea7926b292a9d0ae22
Douglas Farinelli <douglas.farinelli@dressandgo.com.br>
2017-07-01 03:41:41 -0300

    Created core package and abstract model

--- app/core/base.py
+++ app/core/base.py
@@ -1,2 +1,41 @@
 # ! coding: utf-8
 
+from django.db import models
+from django.db.models import manager
+
+
+class BaseQuerySet(manager.QuerySet):
+    """Base Queryset for all models."""
+
+    _only_actives_records = True
+
+    def _clone(self, **kwargs):
+        clone = super(BaseQuerySet, self)._clone(
+            _only_actives_records=self._only_actives_records, **kwargs)
+        if self._only_actives_records:
+            clone.query.add_q(models.Q(is_active=True))
+        return clone
+
+    def all_actives_and_inatives(self):
+        self._only_actives_records = False
+        return self.all()
+
+    def inatives(self):
+        return self.all_actives_and_inatives().filter(is_active=False)
+
+BaseManager = manager.Manager.from_queryset(BaseQuerySet)
+
+
+class BaseModel(models.Model):
+
+    objects = BaseManager()
+
+    is_active = models.BooleanField(default=True, db_index=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
