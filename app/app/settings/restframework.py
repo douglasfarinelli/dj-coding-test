ef263c3eaaa26fbee6845d72e88d24ecdc257426
Douglas Farinelli <douglas.farinelli@dressandgo.com.br>
2017-07-01 15:04:47 -0300

    Support to JSONP

--- app/app/settings/restframework.py
+++ app/app/settings/restframework.py
@@ -7,6 +7,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_jsonp.renderers.JSONPRenderer',
         'rest_framework.parsers.JSONParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (
